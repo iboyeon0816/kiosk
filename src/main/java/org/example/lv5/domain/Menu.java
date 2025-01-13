e3f13c0b4394e8a44532b978bfeae968e83c7992
iboyeon0816 <iboyeon0816@gmail.com>
2025-01-13 19:48:31 +0900

    feat(lv5): 모든 클래스에 캡슐화를 적용하여 필드 직접 접근 차단

---
+++ src/main/java/org/example/lv5/domain/Menu.java
@@ -0,0 +1,49 @@
+package org.example.lv5.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 메뉴를 나타내는 클래스이다.
+ *
+ * - `category`: 메뉴 카테고리의 이름
+ * - `menuItems`: 카테고리 내 `MenuItem`(메뉴 항목)의 리스트
+ */
+public class Menu {
+    private final String category;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String category) {
+        this.category = category;
+        this.menuItems = new ArrayList<>();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    /**
+     * 메뉴 항목을 `menuItems`에 추가한다.
+     *
+     * @param menuItem 추가할 메뉴 항목
+     */
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    /**
+     * 메뉴 항목을 순서대로 출력한다.
+     *
+     * 각 메뉴 항목은 번호와 함께 출력되며, 번호는 1부터 시작한다.
+     */
+    public void printMenuItems() {
+        int size = menuItems.size();
+        for (int i = 1; i <= size; i++) {
+            System.out.println(i + ". " + menuItems.get(i - 1));
+        }
+    }
+}
